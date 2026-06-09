d7aa92a924e936a03bce415adccfbf454343eee7
Athiexx <katheryne.herrera59@gmail.com>
2026-06-09 08:12:10 -0400

    minor change

--- Dynamic OCR PDF Generator/HR-Website/scripts/app-js/authenticate.js
+++ Dynamic OCR PDF Generator/HR-Website/scripts/app-js/authenticate.js
@@ -1,6 +1,6 @@
 import "../library-js/jquery-3.7.1.min.js";
- // Ensure jQuery is imported before using it
-const $ = window.jQuery; 
+// Ensure jQuery is imported before using it
+const $ = window.jQuery;
 
 window.ticket = "";
 
